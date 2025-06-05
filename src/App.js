61c1234b0309cc65c2d3463d172ab3e59490c7bc
Eyerusalem Rufael <jerryrufael32@gmail.com>
2025-06-05 20:12:42 +0300

    Initial commit:Task Manager App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useEffect, useState } from 'react';
+import confetti from "canvas-confetti";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+function App(){
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Buy groceries", completed: false },
+    { id: 2, title: "Read books", completed: true },
+    { id: 3, title: "watching TV", completed: false },
+    { id: 4, title: "PLayin a gitar", completed: false },
+  ]);
+
+  const [newTask, setNewTask] = useState("");
+
+  useEffect(() => {
+    const completedTasks = tasks.filter(task => task.completed).length;
+    if (completedTasks === tasks.length && tasks.length > 0) {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+      });
+    }
+  }, [tasks]);
+
+  const addTask = () => {
+    if (newTask.trim() === "") 
+      return;
+    const newItem = {
+      id: tasks.length + 1,
+      title: newTask,
+      completed: false,
+    };
+    setTasks([...tasks, newItem]);
+    setNewTask("");
+  };
+
+  const toggleTask = (id)=>{
+    setTasks(
+      tasks.map((task)=>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const deleteTask = (id)=>{
+    setTasks(tasks.filter((task)=> task.id !== id));
+  };
+    return(
+      <div className='App'>
+        <h1>Task Manager</h1>
+        <div className='input-section'>
+          <input
+           type='text'
+           placeholder='Add a task'
+           value={newTask}
+           onChange  = {(e) => setNewTask(e.target.value)}/>
+           <button onClick={addTask}>ADD</button>
+
+        </div>
+
+        <ul className='task-list'>
+          {tasks.map((task)=>(<li key={task.id} className={task.completed?'completed':''}>
+          <span onClick={() => toggleTask(task.id)}>{task.title}</span>
+          <button className='delete-btn' onClick={()=>deleteTask(task.id)}>
+            <span role="img" aria-label="delete">🗑️</span>
+          </button>
+         </li>))}
+        </ul>
+      </div>
+    );
+
 }
 
 export default App;
